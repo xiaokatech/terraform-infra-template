e34ba0005fa1e594aade4214f36fc6d56df2e355
Jiean Yang <jiean.yang.freelancer@gmail.com>
2025-07-14 00:55:29 +0800

    Update cdoes

--- modules/06_lambdas_python/2025-07-12-func-01-hello-world/src/build.sh
+++ modules/06_lambdas_python/2025-07-12-func-01-hello-world/src/build.sh
@@ -6,8 +6,12 @@ pip install -r requirement.txt
 echo "====== building Python files... ======"
 rm -rf layer/
 # 创建临时目录结构
-mkdir -p layer/python # Terraform 将使用 layer 目录创建 zip 文件
 # 复制或移动依赖包到正确位置
-cp -r venv/lib layer/python/lib
+# mkdir -p layer/python # Terraform 将使用 layer 目录创建 zip 文件
+# cp -r venv/lib layer/python/lib
+
+mkdir -p layer/python/
+cp -r venv/lib/python3.*/site-packages/* layer/python/
+
 
 
--- modules/06_lambdas_python/2025-07-12-func-01-hello-world/src/index.py
+++ modules/06_lambdas_python/2025-07-12-func-01-hello-world/src/index.py
@@ -1,6 +1,3 @@
-# import psycopg2
-
-
 def lambda_handler(event, context):
     name = event.get("queryStringParameters", {}).get("name", "World")
     return {"statusCode": 200, "body": f"Hello {name}!"}
@@ -8,27 +5,27 @@ def lambda_handler(event, context):
 
 # import os
 # import psycopg2
-# from psycopg2 import pool
+
+# # from psycopg2 import pool
 
 # # 使用连接池管理数据库连接
 # connection_pool = None
 
 
-# def init_db():
-#     global connection_pool
-#     if connection_pool is None:
-#         connection_pool = psycopg2.pool.SimpleConnectionPool(
-#             1,
-#             10,
-#             host=os.getenv("DB_HOST"),
-#             port=os.getenv("DB_PORT"),
-#             database=os.getenv("DB_NAME"),
-#             user=os.getenv("DB_USER"),
-#             password=os.getenv("DB_PASSWORD"),
-#         )
-
-
 # def lambda_handler_db(event, context):
+#     def init_db():
+#         global connection_pool
+#         if connection_pool is None:
+#             connection_pool = psycopg2.pool.SimpleConnectionPool(
+#                 1,
+#                 10,
+#                 host=os.getenv("DB_HOST"),
+#                 port=os.getenv("DB_PORT"),
+#                 database=os.getenv("DB_NAME"),
+#                 user=os.getenv("DB_USER"),
+#                 password=os.getenv("DB_PASSWORD"),
+#             )
+
 #     init_db()
 
 #     try:
